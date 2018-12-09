902aadb85a3369c419c2080a631896b03b57512c
clpagaduan <37362629+clpagaduan@users.noreply.github.com>
2018-12-10 00:14:21 +0800

    Initial commit

---
+++ app/src/test/java/com/example/clpagaduan/dlsu_find_a_roomie/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.clpagaduan.dlsu_find_a_roomie;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
