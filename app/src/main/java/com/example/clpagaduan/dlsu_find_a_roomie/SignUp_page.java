1bcb84bb179afbac103879d42f2ed562326fb08d
jillienetan <31946577+jillienetan@users.noreply.github.com>
2018-12-12 13:49:23 +0800

    jill

---
+++ app/src/main/java/com/example/clpagaduan/dlsu_find_a_roomie/SignUp_page.java
@@ -0,0 +1,13 @@
+package com.example.clpagaduan.dlsu_find_a_roomie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_page);
+    }
+}
