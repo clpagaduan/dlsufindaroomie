44eb35c50091a5aa38f827f6cc404b58ddb3ef87
jillienetan <31946577+jillienetan@users.noreply.github.com>
2018-12-12 14:32:50 +0800

    Update SignUp_page.java

--- app/src/main/java/com/example/clpagaduan/dlsu_find_a_roomie/SignUp_page.java
+++ app/src/main/java/com/example/clpagaduan/dlsu_find_a_roomie/SignUp_page.java
@@ -2,12 +2,24 @@ package com.example.clpagaduan.dlsu_find_a_roomie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class SignUp_page extends AppCompatActivity {
 
+    TextView lastName;
+    TextView firstName;
+    TextView gender;
+    TextView email;
+    TextView username;
+    TextView password;
+    Button back;
+    Button submit;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
     }
+
 }
