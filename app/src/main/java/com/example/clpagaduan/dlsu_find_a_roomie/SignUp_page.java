95d9b2212b3b5c397ce311073fdc9ce65a4ac7db
clpagaduan <37362629+clpagaduan@users.noreply.github.com>
2018-12-14 01:37:13 +0800

    Signup is working, saving at DB

--- app/src/main/java/com/example/clpagaduan/dlsu_find_a_roomie/SignUp_page.java
+++ app/src/main/java/com/example/clpagaduan/dlsu_find_a_roomie/SignUp_page.java
@@ -1,20 +1,30 @@
 package com.example.clpagaduan.dlsu_find_a_roomie;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.util.Log;
+import android.widget.Toast;
 
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+//import com.google.firebase.auth.R;
 
 public class SignUp_page extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -34,14 +44,21 @@ public class SignUp_page extends AppCompatActivity {
     Button btn_foggy;
     TextView txt_condition;
 
+    private ProgressDialog progressDialog;
+    private FirebaseAuth fireBaseAuth;
+
 //    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
 //    DatabaseReference mConditionRef = mDatabase.child("condition");
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+
+        fireBaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
 
         lastName = findViewById(R.id.lastName);
         firstName = findViewById(R.id.firstName);
@@ -58,14 +75,16 @@ public class SignUp_page extends AppCompatActivity {
         btn_sunny = findViewById(R.id.btn_sunny);
         txt_condition = findViewById(R.id.conditionTxt);
 
+
+
 //        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-//        submitbtn.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onclick (View v){
-//                submitPost();
-//            }
-//        });
+        submitbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v){
+                registerUser();
+            }
+        });
 
         test_btn.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -74,6 +93,47 @@ public class SignUp_page extends AppCompatActivity {
             }
         });
     }
+
+    private void registerUser(){
+        String emailStr = email.getText().toString().trim();
+        String passwordStr = password.getText().toString().trim();
+
+        if (TextUtils.isEmpty(emailStr)){
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(passwordStr)){
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("Registering user...");
+        progressDialog.show();
+
+        fireBaseAuth.createUserWithEmailAndPassword(emailStr, passwordStr)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(SignUp_page.this, "Registered Successfully!", Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
+                    } else {
+                        Toast.makeText(SignUp_page.this, "Could not register. Please try again.", Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
+
+                    }
+                }
+            });
+
+    }
+
+//    @Override
+//    public void onClick(View view){
+//        if(view == submitbtn){
+//            registerUser();
+//        }
+//    }
 
     public void basicReadWrite(){
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -88,6 +148,9 @@ public class SignUp_page extends AppCompatActivity {
                 // whenever data at this location is updated.
                 String value = dataSnapshot.getValue(String.class);
                 Log.d(TAG, "Value is: " + value);
+
+                String text = dataSnapshot.getValue(String.class);
+                txt_condition.setText(text);
             }
 
             @Override
